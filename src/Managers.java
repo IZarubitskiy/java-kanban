a90e27621a6f89be540ac02012529e9175e2ea12
IZ <zarubickiy.igor@gmail.com>
2024-09-08 23:12:19 +0300

    ТЗ 6. Первая итерация - исправление ошибок после автоматической проверки

--- src/Managers.java
+++ src/Managers.java
@@ -1,10 +1,10 @@
 public  class Managers {
-     static HistoryManager getDefaultHistory(){
+     static HistoryManager getDefaultHistory() {
          HistoryManager inMemoryHistoryManager = new InMemoryHistoryManager();
          return inMemoryHistoryManager;
      }
 
-     static TaskManager getDefauld(){
+     static TaskManager getDefauld() {
          TaskManager inMemoryTaskManager = new InMemoryTaskManager();
          return  inMemoryTaskManager;
      }
